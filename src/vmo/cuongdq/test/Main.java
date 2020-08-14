87488f0b3f7e70b376a2e0daeded7744e938960e
cuongdang1998 <quoccuong.aha@gmail.com>
2020-08-14 10:13:26 +0700

    add project

---
+++ src/vmo/cuongdq/test/Main.java
@@ -0,0 +1,8 @@
+package vmo.cuongdq.test;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("VietNam");
+        System.out.println("Viet nam tuoi dep");
+    }
+}
